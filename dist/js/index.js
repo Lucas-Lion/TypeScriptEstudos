aa82f829f6fcfa74aa40e29ba4933786954fc4fa
Lucas-Lion <lucaslion350@gmail.com>
2023-10-19 13:41:04 -0300

    estudos TypeScript

---
+++ dist/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let myTuple;
+myTuple = [5, "teste", ["a", "b"]];
+const user = {
+    name: "Lucas",
+    age: 22,
+};
+console.log(user);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let myTuple: [number, string, string[]];
+
+myTuple = [5, "teste", ["a", "b"]];
+
+const user: { name: string, age: number } = {
+	name: "Lucas",
+	age: 22,
+};
+
+console.log(user);
+
